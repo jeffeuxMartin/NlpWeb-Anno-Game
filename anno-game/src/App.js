abd68c6daa752fd33138b541ef58529a5275ebf2
jeffeuxMartin <jeffeuxmlta2020@gmail.com>
2022-12-22 23:32:22 +0800

    只有 router 可以修，其他很不錯了～！

--- anno-game/src/App.js
+++ anno-game/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 import Game from "./components/game/Game";
 import About from "./components/About";
-import Result from './components/game/Result';
+import Result from "./components/game/Result";
 import End from "./components/game/End";
 import QuestionList from "./skeleton_videos/QuestionList";
 import QuestionList2 from "./skeleton_videos/QuestionList2";
@@ -14,113 +14,113 @@ function App() {
   const [showResult, setShowResult] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [isAlertVisible, setIsAlertVisible ] = useState(false);
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
   const [Correctness, setCorrectness] = useState(true);
   const [isHintVisible, setHintVisible] = useState(false);
 
-  let navigate = useNavigate(); 
+  let navigate = useNavigate();
   const questions = QuestionList;
   const questions2 = QuestionList2;
 
   const optionClicked = (isCorrect) => {
-      console.log('clicked');
-      console.log(isCorrect);
-      console.log(questions[currentQuestion].video);
-      if (isCorrect) {
-          setScore(score + 1);
-          setCorrectness(true);
-      } else {
-        setCorrectness(false);
-      }
-      if (currentQuestion + 1 < questions.length) {
-        setCurrentQuestion(currentQuestion + 1);
-       } else {
-        setShowResult(true);
+    console.log("clicked");
+    console.log(isCorrect);
+    console.log(questions[currentQuestion].video);
+    if (isCorrect) {
+      setScore(score + 1);
+      setCorrectness(true);
+    } else {
+      setCorrectness(false);
+    }
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResult(true);
     }
     setIsAlertVisible(true);
     setTimeout(() => {
-            setIsAlertVisible(false);
-          }, 1500);
+      setIsAlertVisible(false);
+    }, 1500);
   };
 
   const optionClickedHint = (isCorrect) => {
-      console.log('clicked');
-      console.log(isCorrect);
-      console.log(currentQuestion);
-      // If correct, score + 1 and go to the next question
-      if (isCorrect) {
-          setScore(score + 1);
-          setCorrectness(true);
-          setHintVisible(false);
-          if (currentQuestion + 1 < questions.length) {
-            setCurrentQuestion(currentQuestion + 1);
-           } else {
-            setShowResult(true);
-        }
-      // If incorrect, ask if the player wants a hint
+    console.log("clicked");
+    console.log(isCorrect);
+    console.log(currentQuestion);
+    // If correct, score + 1 and go to the next question
+    if (isCorrect) {
+      setScore(score + 1);
+      setCorrectness(true);
+      setHintVisible(false);
+      if (currentQuestion + 1 < questions.length) {
+        setCurrentQuestion(currentQuestion + 1);
       } else {
-        setCorrectness(false);
-        setHintVisible(true);
+        setShowResult(true);
       }
+      // If incorrect, ask if the player wants a hint
+    } else {
+      setCorrectness(false);
+      setHintVisible(true);
+    }
     setIsAlertVisible(true);
     setTimeout(() => {
-            setIsAlertVisible(false);
-          }, 1500);
+      setIsAlertVisible(false);
+    }, 1500);
   };
 
   const handleSkip = () => {
     setHintVisible(false);
     setCurrentQuestion(currentQuestion + 1);
-}
+  };
 
   const nextLevel = () => {
     setScore(0);
     setCurrentQuestion(0);
     setShowResult(false);
-    console.log('To the next level!');
-    let path = `/Game2`; 
+    console.log("To the next level!");
+    let path = `/Game2`;
     navigate(path);
-  }
+  };
 
   const showVideos = () => {
-    console.log('End and show videos');
-    let path = `/End`; 
+    console.log("End and show videos");
+    let path = `/End`;
     navigate(path);
-  }
+  };
 
   const restartGame = () => {
-    console.log('restart');
+    console.log("restart");
     setScore(0);
     setCurrentQuestion(0);
     setShowResult(false);
-  }
+  };
 
   return (
     <>
       <Nav />
-      <div className='container-lg'>
+      <div className="container-lg">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route 
+          <Route
             path="/Game"
             element={
               <>
-                { showResult ? (
+                {showResult ? (
                   <div>
                     <Result
-                      congrats={'恭喜完成第一關挑戰！'}
+                      congrats={"恭喜完成第一關挑戰！"}
                       currentScore={score}
                       questions={questions}
                       nextLevel={nextLevel}
                       restartGame={restartGame}
-                      levelup={'前進下一關'}
+                      levelup={"前進下一關"}
                     />
                   </div>
                 ) : (
-                  <div className='row'>
-                    <div className='col-10'>
-                      <Game 
-                        title={' 第一關(warmup)：猜猜我在說什麼？ '}
+                  <div className="row">
+                    <div className="col-10">
+                      <Game
+                        title={" 第一關(warmup)：猜猜我在說什麼？ "}
                         optionClicked={optionClicked}
                         score={score}
                         currentQuestion={currentQuestion}
@@ -132,31 +132,30 @@ function App() {
                       />
                     </div>
                   </div>
-                )
-                }
+                )}
               </>
             }
           />
-          <Route 
+          <Route
             path="/Game2"
             element={
               <>
-                { showResult ? (
+                {showResult ? (
                   <div>
                     <Result
-                      congrats={'恭喜完成第二關挑戰！'}
+                      congrats={"恭喜完成第二關挑戰！"}
                       currentScore={score}
                       questions={questions2}
                       nextLevel={showVideos}
                       restartGame={restartGame}
-                      levelup={'揭曉影片原檔'}
+                      levelup={"揭曉影片原檔"}
                     />
                   </div>
                 ) : (
-                  <div className='row'>
-                    <div className='col-10'>
-                      <Game 
-                        title={' 第二關：猜猜我是誰？ '}
+                  <div className="row">
+                    <div className="col-10">
+                      <Game
+                        title={" 第二關：猜猜我是誰？ "}
                         optionClicked={optionClickedHint}
                         score={score}
                         currentQuestion={currentQuestion}
@@ -167,24 +166,24 @@ function App() {
                         needTTS={false}
                         handleSkip={handleSkip}
                         nCol={"col-3"}
-                        closePopup={()=>setHintVisible(false)}
+                        closePopup={() => setHintVisible(false)}
                       />
                     </div>
                   </div>
-                )
-                }
+                )}
               </>
             }
           />
-          <Route path="/End" element={
-                                <End 
-                                  currentQuestion={currentQuestion}
-                                  questions={questions}
-                                />} />
+          <Route
+            path="/End"
+            element={
+              <End currentQuestion={currentQuestion} questions={questions} />
+            }
+          />
           <Route path="/About" element={<About />} />
         </Routes>
-     </div>
-   </>
+      </div>
+    </>
   );
 }
 
